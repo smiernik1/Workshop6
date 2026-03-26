e6ad2ed51d2bad2cceaf0f3b0c2c607f1df1841e
smiernik1 <smiernik@visa.com>
2026-03-26 22:53:58 +0100

    Stworzenie projektu opartego o SpringBoot, uzupełnienie properties o dane do połączenia z bazą danych workshopFinal

---
+++ BookAPI/src/test/java/pl/coderslab/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
